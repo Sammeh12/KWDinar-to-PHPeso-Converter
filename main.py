fa38cd7dae4f79a7a363e6d2625f9afe00f77865
Sammeh12 <samuel.gamay12@gmail.com>
2023-07-16 15:55:26 +0800

    KWDinar to PHPeso Converter

---
+++ main.py
@@ -0,0 +1,5 @@
+kuwaitiDinar = int(input("Enter Kuwaiti Dinar: "))
+philippinePeso = 177.75
+dinarToPeso = kuwaitiDinar * philippinePeso
+
+print("You have","{:,}".format(dinarToPeso),"in Philippine Peso")
